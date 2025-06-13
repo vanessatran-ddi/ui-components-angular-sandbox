a5a27c31454a5d30382477caf64e04fec00d8829
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-06-13 15:09:45 -0600

    update code snippet 383

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent { 
+export class AppComponent {
+  fullName: string = "";
+  accountNumber: string = "";
+  price: string = "";
+  quantity: string = "";
+
+  onChange(event: Event) {
+    const customEvent = event as CustomEvent;
+    const { name, value } = customEvent.detail;
+
+    switch(name) {
+      case 'fullName':
+        this.fullName = value;
+        break;
+      case 'accountNumber':
+        this.accountNumber = value;
+        break;
+      case 'price':
+        this.price = value;
+        break;
+      case 'quantity':
+        this.quantity = value;
+        break;
+    }
+  }
 }
