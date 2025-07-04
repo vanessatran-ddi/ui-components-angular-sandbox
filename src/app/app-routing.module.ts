70235132b2d9c08b6f286421d56ad057fcef1b0e
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-07-04 11:05:06 -0600

    add example code that sub form has a bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SupportOrderDetailsComponent } from './public-form/SupportOrderDetails';
 
 const routes: Routes = [
-  // { path: 'home', component: HomeComponent },
+  { path: '', component: SupportOrderDetailsComponent },
+  { path: 'fsos', component: SupportOrderDetailsComponent }
 ];
 
 @NgModule({
